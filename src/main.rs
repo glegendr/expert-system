e6265c0c41cf023074577720314becfa9f9d7ca1
glegendr <glegendr@student.42.fr>
2022-05-31 21:02:11 +0200

    add =

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::collections::{HashMap, HashSet};
+use std::fmt;
+use colored::Colorize;
+
+const RESERVED_WORDS: [&'static str; 16] = [
+    "and",
+    "&",
+    "or",
+    "|",
+    "xor",
+    "^",
+    "equal",
+    "=",
+    "not",
+    "!",
+    "then",
+    "=>",
+    "<=>",
+    "if",
+    "def",
+    "?"
+];
 
 #[derive(Debug)]
 struct Variable {
@@ -9,6 +30,18 @@ struct Variable {
     requested: bool,
     alias_true: Option<String>,
     alias_false: Option<String>
+}
+
+impl fmt::Display for Variable {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        
+        let is_ok = |v| if v {
+            "✓".green()
+        } else {
+            "x".red()
+        };
+        write!(f, "{}|{}|{}|{}|{}", is_ok(self.value), is_ok(self.locked), is_ok(self.requested), self.alias_true.clone().unwrap_or_default(), self.alias_false.clone().unwrap_or_default())
+    }
 }
 
 impl Variable {
@@ -67,6 +100,7 @@ fn line_to_chunk(line: &str) -> Result<Vec<String>, String> {
             },
             ('"', true) => {
                 scoped = false;
+                acc.push(String::default())
             },
             _ => acc.get_mut(accessor).unwrap().push(c),
         };
@@ -101,8 +135,18 @@ fn def_var(chunks: &Vec<String>, variables: &mut HashMap<String, Variable>) -> R
                     }
                 }
             },
-            1 => var.alias_true = Some(String::from(chunk)),
-            2 => var.alias_false = Some(String::from(chunk)),
+            1 => {
+                if RESERVED_WORDS.contains(&chunk.as_str()) {
+                    Err(format!("{chunk} is a reserved word"))?
+                }
+                var.alias_true = Some(String::from(chunk))
+            },
+            2 => {
+                if RESERVED_WORDS.contains(&chunk.as_str()) {
+                    Err(format!("{chunk} is a reserved word"))?
+                }
+                var.alias_false = Some(String::from(chunk))
+            },
             _ => Err(format!("unexpected {chunk} in def line"))?
         }
     }
@@ -115,6 +159,36 @@ fn def_var(chunks: &Vec<String>, variables: &mut HashMap<String, Variable>) -> R
     Ok(())
 }
 
+fn user_set(chunks: &Vec<String>, variables: &mut HashMap<String, Variable>) -> Result<(), String> {
+    if chunks.len() == 1 {
+        Err(String::from("= line expect at least 1 variable"))?
+    }
+    let mut iter_chunks = chunks.iter();
+    iter_chunks.next();
+    for chunk in iter_chunks {
+        if let Some(var) = variables.get_mut(chunk) {
+            var.value = true;
+            var.locked = true;
+        } else {
+            for c in chunk.chars() {
+                if let Some(var) = variables.get_mut(&c.to_string()) {
+                    // If var.locked then warning
+                    var.value = true;
+                    var.locked = true;
+                } else if let Some((_, var)) = variables.iter_mut().find(|(_, v)| v.alias_true.clone().unwrap_or_default() == *chunk) {
+                    var.value = true;
+                    var.locked = true;
+                } else {
+                    variables.insert(c.to_string(), Variable::insert());
+                }
+            }
+        }
+
+    }
+    // println!("{chunks:?}");
+    Ok(())
+}
+
 fn fill_maps(variables: &mut HashMap<String, Variable>, rules: &mut HashSet<String>, file: &str) -> Result<(), String> {
     let lines = to_splited_string(file)?;
     for line in lines {
@@ -122,9 +196,9 @@ fn fill_maps(variables: &mut HashMap<String, Variable>, rules: &mut HashSet<Stri
         let chunks = line_to_chunk(&line)?;
         if let Some(first) = chunks.iter().next() {
             match first.as_str() {
-                "def" => {def_var(&chunks, variables)?},
+                "def" => def_var(&chunks, variables)?,
                 "if" => {},
-                "=" => {},
+                "=" => user_set(&chunks, variables)?,
                 "?" => {},
                 _ => {}
             }
@@ -171,10 +245,18 @@ fn fill_maps(variables: &mut HashMap<String, Variable>, rules: &mut HashSet<Stri
 }
 
 fn main() -> Result<(), String> {
-    let file = "frog";
+    let file = "test_files/frog";
     let mut variables = HashMap::new();
     let mut rules = HashSet::new();
     fill_maps(&mut variables, &mut rules,  file)?;
-    println!("Variables: {variables:?}\nRules:     {rules:?}");
+    print_vars(&variables);
+    // println!("Variables: {variables:?}\nRules:     {rules:?}");
     Ok(())
+}
+
+fn print_vars(variables: &HashMap<String, Variable>)  {
+    println!("name v|l|r|true|false");
+    for (k, v) in variables.iter() {
+        println!("{k} -> {v}")
+    }
 }
