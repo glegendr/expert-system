fefa10aaaf24f180a95342c8c3860f43cadd1e7a
Lorenzo JESU <lojesu@e2r11p2.clusters.42paris.fr>
2022-06-03 19:27:01 +0200

    up algo

--- src/algo.rs
+++ src/algo.rs
@@ -8,6 +8,8 @@ pub fn algo_v1() {
     rules.push(String::from("A B & => M"));
     rules.push(String::from("F M | => G"));
     rules.push(String::from("C => Y"));
+    rules.push(String::from("U V | => Z"));
+    rules.push(String::from("A B & => Z"));
     let mut facts = HashMap::new();
     facts.insert('A',(true, true));
     facts.insert('B',(true, true));
@@ -16,7 +18,9 @@ pub fn algo_v1() {
     facts.insert('M',(false, false));
     facts.insert('G',(false, false));
     facts.insert('Y',(false, false));
-    println!("{}", search_querie('Y', &mut facts, &rules));
+    facts.insert('U',(false, false));
+    facts.insert('V',(false, false));
+    facts.insert('Z',(false, false));
     println!("{}", search_querie('G', &mut facts, &rules));
 }
 
@@ -29,7 +33,7 @@ fn search_querie(querie: char, facts: &mut HashMap<char, (bool, bool)>, rules: &
         }
         _ => ()
     }
-    //TO UP -> check all seconds parts of RULES & keep RULES with querie in second part
+    //TOUP -> check all seconds parts of RULES & keep RULES with querie in second part
     let mut querie_rules = Vec::new();
     for rule in rules.iter() {
         if rule.chars().last() == Some(querie) {
@@ -37,24 +41,54 @@ fn search_querie(querie: char, facts: &mut HashMap<char, (bool, bool)>, rules: &
         }
     }
     if querie_rules.len() == 0 {
-        facts.remove(&querie);
         facts.insert(querie, (false, true));
         return false
     }
-    //TODO -> replace querie_rules[0] to function who choose the simpliest
-    for c in querie_rules[0].chars() {
-        if c.is_alphabetic() == true {
-            if let Some((_, lock)) = facts.get_mut(&c) {
-                if *lock == false {
-                    facts.insert(c, (search_querie(c, &mut facts.clone(), rules), true));
-                    //change facts with new value and lock value
+    //TODO -> do all rule until true
+    let mut i = 0;
+    let mut all_fact_is_true = false;
+    while i < querie_rules.len() {
+        for c in querie_rules[i].chars() {
+            if c.is_alphabetic() == true {
+                if let Some((_, lock)) = facts.get_mut(&c) {
+                    if *lock == false {
+                        match search_querie(c, &mut facts.clone(), rules) {
+                            true => {
+                                facts.insert(c, (true, true));
+                                all_fact_is_true = true;
+                            }
+                            _ => {
+                                all_fact_is_true = false;
+                                continue
+                            }
+                        };
+                    } else {
+                        all_fact_is_true = true;
+                    }
                 }
+            } else if c == '=' {
+                break;
             }
-        } else if c == '=' {
+        }
+        if all_fact_is_true == true {
             break;
         }
+        i += 1;
     }
-    eval_formula(rules_to_bool_str(querie_rules[0].clone(), facts).as_str())
+    if all_fact_is_true == false && i == querie_rules.len() {
+        facts.insert(querie, (false, true));
+        return false
+    }
+    match eval_formula(rules_to_bool_str(querie_rules[i].clone(), facts).as_str()) {
+        true => {
+            facts.insert(querie, (true, true));
+            true
+        },
+        _ => {
+            facts.insert(querie, (false, true));
+            false
+        }
+    }
 }
 
 fn rules_to_bool_str(rule: String, facts: &mut HashMap<char, (bool, bool)>) -> String {
@@ -67,7 +101,7 @@ fn rules_to_bool_str(rule: String, facts: &mut HashMap<char, (bool, bool)>) -> S
                     false => result.push('0')
                 }
             }
-        } else if c == '&' || c == '|' || c == '^' || c == '!' {
+        } else if "&|^!".contains(c) {
                 result.push(c);
         } else if c == '=' {
             break;
